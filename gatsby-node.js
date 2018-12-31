50038f90ec70387cc1f5b4170bc49b162fc4ef7d
Mark Sta Ana <booyaabooyaabooyaa@gmail.com>
2018-12-31 17:19:09 +0000

    Add handler for WordPress posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,42 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path')
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
 
-// You can delete this file if you're not using it
+  const createWpPosts = new Promise((resolve, reject) => {
+    const query = graphql(`
+      {
+        allWordpressPost {
+          edges {
+            node {
+              id
+              slug
+            }
+          }
+        }
+      }
+    `)
+
+    query.then(result => {
+      if (result.errors) {
+        console.error(results.errors)
+        reject(result.error)
+      }
+
+      const postEdges = result.data.allWordpressPost.edges
+
+      postEdges.forEach(edge => {
+        createPage({
+          path: `/${edge.node.slug}`,
+          component: path.resolve(`./src/templates/post.js`),
+          context: {
+            id: edge.node.id,
+          },
+        })
+      })
+
+      resolve()
+    }) // query.then
+  }) // createWpPosts
+
+  return Promise.all([createWpPosts])
+} // createPages
